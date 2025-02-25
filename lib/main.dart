92c61d88b16a5c2cd9dd0f52f78dc2eac7b36e87
RENATA_RONQUILLO_LOPEZ <a.22308051281307@cbtis128.edu.mx>
2025-02-25 16:58:15 +0000

    Subir

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Mis imagenes",
+            style: TextStyle(
+              fontSize: 25,
+              fontStyle: FontStyle.italic,
+              color: Color(0xff000000),
+            ),
+          ),
+          backgroundColor: Color(0xff8e1010),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                ' Renata Ronquillo Lopez Mat: 22308051281307 6to J ',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff680505),
+                ),
+              ),
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+              Image.network(
+                'https://raw.githubusercontent.com/Renata-Ronquillo-5-J/Imagenes-para-app-flutter/refs/heads/main/Image1.jpg', // URL de la primera imagen
+                height: 150,
+                width: 150,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              const Text(
+                'Logo de miniso ',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff680505),
+                ),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Renata-Ronquillo-5-J/Imagenes-para-app-flutter/refs/heads/main/Imagen2.jpg', // URL de la segunda imagen
+                height: 150,
+                width: 150,
+              ),
+              const SizedBox(height: 20), // Espacio entre las imágenes
+              const Text(
+                'Tienda fisica por adentro',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  color: Color(0xff680505),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
+    ); //Material App
   }
 }
